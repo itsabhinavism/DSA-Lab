73aae072b27dbe4823fc218038c7e2210102b087
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-09-02 11:00:06 +0530

    save

--- Practice/p1.cpp
+++ Practice/p1.cpp
@@ -1,53 +1,85 @@
 #include <iostream>
-
-// Function to check if a number is prime
-bool isPrime(int num)
-{
-    if (num <= 1)
-        return false;
-    for (int i = 2; i * i <= num; ++i)
-    {
-        if (num % i == 0)
-            return false;
-    }
-    return true;
-}
-
-// Function to calculate the sum of prime elements in an array
-int sumOfPrimes(int *arr, int size)
-{
-    int sum = 0;
-    for (int i = 0; i < size; ++i)
-    {
-        if (isPrime(arr[i]))
-        {
-            sum += arr[i];
-        }
-    }
-    return sum;
-}
-
+using namespace std;
 int main()
 {
-    int size;
-    std::cout << "Enter size of the array: ";
-    std::cin >> size;
-
-    // Dynamically allocate memory for the array
-    int *arr = new int[size];
-
-    std::cout << "Enter array elements: ";
-    for (int i = 0; i < size; ++i)
+    int r, c;
+    int size = 0;
+    cout << "enter rows : ";
+    cin >> r;
+    cout << "enter columns : ";
+    cin >> c;
+    int arr[r][c];
+    for (int i = 0; i < r; i++)
     {
-        std::cin >> arr[i];
+        for (int j = 0; j < c; j++)
+        {
+            cin >> arr[i][j];
+        }
     }
-
-    // Calculate the sum of prime elements in the array
-    int sum = sumOfPrimes(arr, size);
-    std::cout << "Sum = " << sum << std::endl;
-
-    // Deallocate the memory for the array
-    delete[] arr;
-
-    return 0;
+    cout << " the matrix you entered";
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            cout << arr[i][j];
+            if (arr[i][j] != 0)
+            {
+                size++;
+            }
+        }
+        cout << endl;
+    }
+    cout << "no of non zero values";
+    cout << size << endl;
+    cout << "sparse matrix " << endl;
+    // sparse matrix
+    int s_arr[size][3];
+    int q = 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (arr[i][j] != 0)
+            {
+                s_arr[q][0] = i;
+                s_arr[q][1] = j;
+                s_arr[q][2] = arr[i][j];
+                q++;
+            }
+        }
+    }
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << s_arr[i][j];
+        }
+        cout << endl;
+    }
+    cout << " transpose" << endl;
+    // transpose
+    int m[size][3];
+    int a = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (s_arr[j][1] == i)
+            {
+                m[a][1] = s_arr[j][0];
+                m[a][0] = s_arr[j][1];
+                m[a][2] = s_arr[j][2];
+                a++;
+            }
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << m[i][j];
+        }
+        cout << endl;
+    }
 }
