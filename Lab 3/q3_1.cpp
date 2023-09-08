6d72030229872b2bed7ac004d36b52cd3f18c118
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-09-09 00:51:53 +0530

    save

--- Lab 3/q3_1.cpp
+++ Lab 3/q3_1.cpp
@@ -1,68 +1,86 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
-
-struct Element {
-    int row;
-    int col;
-    int value;
-};
-
-struct SparseMatrix {
-    int numRows;
-    int numCols;
-    int numElements;
-    vector<Element> elements;
-};
-
-SparseMatrix transposeSparseMatrix(const SparseMatrix& inputMatrix) {
-    SparseMatrix transposedMatrix;
-    transposedMatrix.numRows = inputMatrix.numCols;
-    transposedMatrix.numCols = inputMatrix.numRows;
-    transposedMatrix.numElements = inputMatrix.numElements;
-    
-    for (int i = 0; i < inputMatrix.numCols; ++i) {
-        for (int j = 0; j < inputMatrix.numElements; ++j) {
-            if (inputMatrix.elements[j].col == i) {
-                Element transposedElement;
-                transposedElement.row = inputMatrix.elements[j].col;
-                transposedElement.col = inputMatrix.elements[j].row;
-                transposedElement.value = inputMatrix.elements[j].value;
-                transposedMatrix.elements.push_back(transposedElement);
+int main()
+{
+    int r, c;
+    int size = 0;
+    cout << "Enter Rows : ";
+    cin >> r;
+    cout << "Enter Columns : ";
+    cin >> c;
+    int arr[r][c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    cout << "The matrix you entered : ";
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            cout << arr[i][j];
+            if (arr[i][j] != 0)
+            {
+                size++;
+            }
+        }
+        cout << endl;
+    }
+    cout << "\nNumber of non zero values : ";
+    cout << size << endl;
+    // cout<< "\n ";
+    cout << "\nSparse Matrix : " << endl;
+    // sparse matrix
+    int s_arr[size][3];
+    int q = 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (arr[i][j] != 0)
+            {
+                s_arr[q][0] = i;
+                s_arr[q][1] = j;
+                s_arr[q][2] = arr[i][j];
+                q++;
             }
         }
     }
-    
-    return transposedMatrix;
-}
-
-int main() {
-    SparseMatrix inputMatrix;
-    
-    // Input the dimensions and non-zero elements of the matrix
-    cout << "Enter the number of rows: ";
-    cin >> inputMatrix.numRows;
-    cout << "Enter the number of columns: ";
-    cin >> inputMatrix.numCols;
-    cout << "Enter the number of non-zero elements: ";
-    cin >> inputMatrix.numElements;
-    
-    for (int i = 0; i < inputMatrix.numElements; ++i) {
-        Element element;
-        cout << "Enter element " << i + 1 << " (row, column, value): ";
-        cin >> element.row >> element.col >> element.value;
-        inputMatrix.elements.push_back(element);
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << s_arr[i][j];
+        }
+        cout << endl;
     }
-    
-    SparseMatrix transposedMatrix = transposeSparseMatrix(inputMatrix);
-    
-    // Output the transposed matrix
-    cout << "\nTransposed Matrix (in 3-tuple format):\n";
-    cout << "Rows: " << transposedMatrix.numRows << ", Columns: " << transposedMatrix.numCols << endl;
-    for (const Element& element : transposedMatrix.elements) {
-        cout << element.row << " " << element.col << " " << element.value << endl;
+    cout << "\nTranspose : " << endl;
+    // transpose
+    int m[size][3];
+    int a = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (s_arr[j][1] == i)
+            {
+                m[a][1] = s_arr[j][0];
+                m[a][0] = s_arr[j][1];
+                m[a][2] = s_arr[j][2];
+                a++;
+            }
+        }
+        // cout << endl;
     }
-    
-    return 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << m[i][j];
+        }
+        cout << endl;
+    }
 }
