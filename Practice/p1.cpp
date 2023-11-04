c4a39e818e0bc258f3cb535c5e85d64387a9683b
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-11-04 10:24:00 +0530

    save

--- Practice/p1.cpp
+++ Practice/p1.cpp
@@ -1,85 +1,30 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
 int main()
 {
-    int r, c;
-    int size = 0;
-    cout << "enter rows : ";
-    cin >> r;
-    cout << "enter columns : ";
-    cin >> c;
-    int arr[r][c];
-    for (int i = 0; i < r; i++)
-    {
-        for (int j = 0; j < c; j++)
-        {
-            cin >> arr[i][j];
-        }
-    }
-    cout << " the matrix you entered";
-    for (int i = 0; i < r; i++)
-    {
-        for (int j = 0; j < c; j++)
-        {
-            cout << arr[i][j];
-            if (arr[i][j] != 0)
-            {
-                size++;
-            }
-        }
-        cout << endl;
-    }
-    cout << "no of non zero values";
-    cout << size << endl;
-    cout << "sparse matrix " << endl;
-    // sparse matrix
-    int s_arr[size][3];
-    int q = 0;
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            if (arr[i][j] != 0)
-            {
-                s_arr[q][0] = i;
-                s_arr[q][1] = j;
-                s_arr[q][2] = arr[i][j];
-                q++;
-            }
-        }
-    }
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            cout << s_arr[i][j];
-        }
-        cout << endl;
-    }
-    cout << " transpose" << endl;
-    // transpose
-    int m[size][3];
-    int a = 0;
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            if (s_arr[j][1] == i)
-            {
-                m[a][1] = s_arr[j][0];
-                m[a][0] = s_arr[j][1];
-                m[a][2] = s_arr[j][2];
-                a++;
-            }
-        }
-        cout << endl;
-    }
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            cout << m[i][j];
-        }
-        cout << endl;
-    }
+    Node *head = NULL;
+    Node *second = NULL;
+    Node *third = NULL;
+
+    head = new Node;
+    second = new Node;
+    third = new Node;
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    return 0;
 }
---
+++ Practice/p3.c
@@ -0,0 +1,236 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 100
+
+struct Deque
+{
+    int size;
+    int queue[MAX_SIZE];
+    int front, rear;
+};
+
+void initializeDeque(struct Deque *deque, int size)
+{
+    deque->size = size;
+    deque->front = -1;
+    deque->rear = -1;
+}
+
+int isFull(struct Deque *deque)
+{
+    return ((deque->front == 0 && deque->rear == deque->size - 1) || (deque->rear == (deque->front - 1) % (deque->size - 1)));
+}
+
+int isEmpty(struct Deque *deque)
+{
+    return (deque->front == -1);
+}
+
+void enqueueFront(struct Deque *deque, int item)
+{
+    if (isFull(deque))
+    {
+        printf("Queue is full. Cannot enqueue at the front.\n");
+    }
+    else
+    {
+        if (isEmpty(deque))
+        {
+            deque->front = 0;
+            deque->rear = 0;
+        }
+        else if (deque->front == 0)
+        {
+            deque->front = deque->size - 1;
+        }
+        else
+        {
+            deque->front = (deque->front - 1) % (deque->size - 1);
+        }
+        deque->queue[deque->front] = item;
+    }
+}
+
+void enqueueRear(struct Deque *deque, int item)
+{
+    if (isFull(deque))
+    {
+        printf("Queue is full. Cannot enqueue at the rear.\n");
+    }
+    else
+    {
+        if (isEmpty(deque))
+        {
+            deque->front = 0;
+            deque->rear = 0;
+        }
+        else if (deque->rear == deque->size - 1)
+        {
+            deque->rear = 0;
+        }
+        else
+        {
+            deque->rear = (deque->rear + 1) % (deque->size - 1);
+        }
+        deque->queue[deque->rear] = item;
+    }
+}
+
+int dequeueFront(struct Deque *deque)
+{
+    if (isEmpty(deque))
+    {
+        printf("Queue is empty. Cannot dequeue from the front.\n");
+        return -1;
+    }
+    else
+    {
+        int item = deque->queue[deque->front];
+        if (deque->front == deque->rear)
+        {
+            deque->front = -1;
+            deque->rear = -1;
+        }
+        else
+        {
+            deque->front = (deque->front + 1) % (deque->size - 1);
+        }
+        return item;
+    }
+}
+
+int dequeueRear(struct Deque *deque)
+{
+    if (isEmpty(deque))
+    {
+        printf("Queue is empty. Cannot dequeue from the rear.\n");
+        return -1;
+    }
+    else
+    {
+        int item = deque->queue[deque->rear];
+        if (deque->front == deque->rear)
+        {
+            deque->front = -1;
+            deque->rear = -1;
+        }
+        else if (deque->rear == 0)
+        {
+            deque->rear = deque->size - 1;
+        }
+        else
+        {
+            deque->rear = (deque->rear - 1) % (deque->size - 1);
+        }
+        return item;
+    }
+}
+
+void traverse(struct Deque *deque)
+{
+    if (isEmpty(deque))
+    {
+        printf("Queue is empty.\n");
+    }
+    else
+    {
+        int i = deque->front;
+        printf("Deque contents: ");
+        while (1)
+        {
+            printf("%d ", deque->queue[i]);
+            if (i == deque->rear)
+            {
+                break;
+            }
+            i = (i + 1) % (deque->size - 1);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    struct Deque deque;
+    int size;
+
+    printf("Enter the size of the deque: ");
+    scanf("%d", &size);
+
+    initializeDeque(&deque, size);
+
+    while (1)
+    {
+        int choice, item;
+        printf("\n1. Enqueue at front\n");
+        printf("2. Enqueue at rear\n");
+        printf("3. Dequeue from front\n");
+        printf("4. Dequeue from rear\n");
+        printf("5. Traverse\n");
+        printf("6. Is Empty\n");
+        printf("7. Is Full\n");
+        printf("8. Quit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("Enter the item to enqueue at the front: ");
+            scanf("%d", &item);
+            enqueueFront(&deque, item);
+            break;
+        case 2:
+            printf("Enter the item to enqueue at the rear: ");
+            scanf("%d", &item);
+            enqueueRear(&deque, item);
+            break;
+        case 3:
+            item = dequeueFront(&deque);
+            if (item != -1)
+            {
+                printf("Dequeued from front: %d\n", item);
+            }
+            break;
+        case 4:
+            item = dequeueRear(&deque);
+            if (item != -1)
+            {
+                printf("Dequeued from rear: %d\n", item);
+            }
+            break;
+        case 5:
+            traverse(&deque);
+            break;
+        case 6:
+            if (isEmpty(&deque))
+            {
+                printf("Queue is empty.\n");
+            }
+            else
+            {
+                printf("Queue is not empty.\n");
+            }
+            break;
+        case 7:
+            if (isFull(&deque))
+            {
+                printf("Queue is full.\n");
+            }
+            else
+            {
+                printf("Queue is not full.\n");
+            }
+            break;
+        case 8:
+            printf("Exiting the program.\n");
+            exit(0);
+        default:
+            printf("Invalid choice. Please enter a valid option.\n");
+        }
+    }
+
+    return 0;
+}
